c77297bfccbd581b62f5ccea1d4332be7fd56c72
Calebe Rodrigues <calebebrodrigues@gmail.com>
2020-07-25 23:15:27 -0300

    Classes criadas

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class ImportedProduct extends Product{
+
+	private Double customsFee;
+	
+	public ImportedProduct() {
+		super();
+	}
+
+	public ImportedProduct(String name, Double price, Double customsFree) {
+		super(name, price);
+		this.customsFee = customsFree;
+	}
+
+	public Double getCustomsFree() {
+		return customsFee;
+	}
+
+	public void setCustomsFree(Double customsFree) {
+		this.customsFee = customsFree;
+	}
+	
+	@Override
+	public String priceTag() {
+		return name + " $" + price + " (Custom fee: $" + customsFee + ")";
+	}
+}
+
